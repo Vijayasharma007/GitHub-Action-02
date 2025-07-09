8fa46d7885f0df7acac5879fd7c2e87c26e68f1c
vijayasharma <114993621+Vijayasharma007@users.noreply.github.com>
2025-07-09 12:24:41 +0530

    updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
     user: 'admin',
     pass: 'admin123',
     useNewUrlParser: true,
